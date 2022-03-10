7ab4fd234ce90e643b63fb5a6d8aeaa1da789dfc
Zas Mama <104ambrown@gmail.com>
2022-03-10 01:50:45 -0500

    get request to query sight

--- Instructions/static/js/logic.js
+++ Instructions/static/js/logic.js
@@ -6,7 +6,7 @@
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
 
 // writing a function for the map
-function creatMyMap(earthquakes) {
+function createMyMap(earthquakes) {
     //Establishing bas layers declaring variablkes
     var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -17,19 +17,37 @@ function creatMyMap(earthquakes) {
       });
 
     // declaring variable for basemap object
-    var baseMap = {"Strret Map": street, "Topographic Map": topo};
+    var baseMap = {"Street Map": street, "Topographic Map": topo};
 
     // variable for earthquake overlay on my map
     var overlayMap = { Earthquakes: earthquakes};
 
 
 // Creating a variable for my map establishing where the center is and level of zoom
-var myMAp = L.map("basicMap", {
+var myMap = L.map("basicMap", {
     center: [50.000, -50.000],
     zoom: 3,
     // what layers I want to overlay on my map
     layers: [street, earthquakes]
 });
+
+// D# get request tot hte url we're querying
+d3.json(url).then(function (data) {
+    console.log(data)
+    function styleInfo(feature) {
+        return {
+            opacity: 1, 
+            fillOpacity: 1, 
+            // determined by the depth of earthquake
+            fillColor: getColor(feature.properties.depth),
+            color: "green",
+            // determined by quakes magnitude
+            radius: getRadius(feature.properties.mag),
+            stroek: true,
+            weight: 0/5
+        };
+    }
+})
 
 // adding tile layer to the map
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
